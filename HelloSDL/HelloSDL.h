b4ed443aecfb8b667e837da34ee59560be909e0e
Jeff <jeff@cogentcoder.com>
2021-04-13 01:13:57 -0400

    Added HelloSDL

---
+++ HelloSDL/HelloSDL.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+
+// C++ header files
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
